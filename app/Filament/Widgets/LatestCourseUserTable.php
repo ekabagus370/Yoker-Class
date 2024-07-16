96ebcfd276efa60fd278848f4c3c9de257010f12
RizalArdiansyah31 <rizalardiansyahnew@gmail.com>
2024-07-16 21:09:06 +0700

    fix: dashboard

--- app/Filament/Widgets/LatestCourseUserTable.php
+++ app/Filament/Widgets/LatestCourseUserTable.php
@@ -2,8 +2,10 @@
 
 namespace App\Filament\Widgets;
 
+use App\Filament\Resources\CourseClassResource;
 use App\Filament\Resources\CourseResource;
 use App\Models\Course;
+use App\Models\CourseClass;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
@@ -13,11 +15,11 @@ class LatestCourseUserTable extends BaseWidget
     protected int | string | array $columnSpan = 'full';
 
     protected static ?int $sort = 2;
-    
+
     public function table(Table $table): Table
     {
         return $table
-            ->query(CourseResource::getEloquentQuery())
+            ->query(CourseClassResource::getEloquentQuery())
             ->defaultPaginationPageOption(5)
             ->defaultSort('created_at', 'desc')
             ->columns([
@@ -29,20 +31,29 @@ class LatestCourseUserTable extends BaseWidget
                 Tables\Columns\TextColumn::make('name')
                     ->sortable()
                     ->searchable(),
-                Tables\Columns\TextColumn::make('code'),
-                Tables\Columns\TextColumn::make('teacher.name')
+                Tables\Columns\TextColumn::make('description')
+                    ->html()
+                    ->wrap()
+                    ->lineClamp(2),
+                Tables\Columns\TextColumn::make('room')
                     ->sortable()
                     ->searchable(),
-                Tables\Columns\TextColumn::make('students_count')->counts('students'),
+                Tables\Columns\TextColumn::make('code')
+                    ->copyable()
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('course.name')
+                    ->sortable()
+                    ->searchable(),
             ])
             ->actions([
                 Tables\Actions\Action::make('open')
-                    ->url(function (Course $record): string {
-                        if (auth()->user()->role = 'admin') {
-                            return  CourseResource::getUrl('edit', ['record' => $record]);
-                        } else {
-                            return  CourseResource::getUrl('view', ['record' => $record]);
-                        }
+                    ->url(function (CourseClass $record): string {
+                        // if (auth()->user()->role = 'admin') {
+                        return  CourseResource::getUrl('edit', ['record' => $record]);
+                        // } else {
+                        // return  CourseResource::getUrl('view', ['record' => $record]);
+                        // }
                     }),
             ]);
     }
